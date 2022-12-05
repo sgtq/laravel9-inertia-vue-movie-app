e5ce8b79834141c9e6429fa48a25047fc56689bb
Gaston <gaston.quittner@gmail.com>
2022-12-04 18:27:00 -0600

    Create Models and Migrations

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -54,7 +54,7 @@ const logout = () => {
                                 </NavLink>
                                 <NavLink
                                     v-if="$page.props.is_admin"
-                                    :href="route('dashboard')" :active="route().current('dashboard')">
+                                    :href="route('admin.index')" :active="route().current('admin.index')">
                                     Admin
                                 </NavLink>
                             </div>
